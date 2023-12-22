31a066a947228ee5e6398d38a5591a0b9000877f
abbosnasriddinov6 <abbosbeknasriddinov001@gmail.com>
2023-12-23 01:29:38 +0500

    done

---
+++ modal.js
@@ -0,0 +1,13 @@
+const btnOpen = document.getElementById('btn-open');
+const btnClose = document.getElementById('btn-close');
+const modal = document.querySelector('.modal');
+
+btnOpen.addEventListener('click', function (e) {
+  modal.classList.add('open');
+});
+
+btnClose.addEventListener('click', function (e) {
+  modal.classList.remove('open');
+});
+
+
